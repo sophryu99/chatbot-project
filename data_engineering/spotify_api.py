443f17a46a0a8cf284d830ce466f5f8f18b69b5a
sophryu99 <sehwa_ryu@berkeley.edu>
2020-11-11 01:00:50 +0900

    Update: Spotify API

--- data_engineering/spotify_api.py
+++ data_engineering/spotify_api.py
@@ -8,124 +8,106 @@ import csv
 
 
 
-def main():
 
-    ## db connect
+def main():
+    
     try:
-        conn = pymysql.connect(host, user=username, passwd=password,
-                               db=database, port=port, use_unicode=True, charset="utf8")
+        conn = pymysql.connect(host, user=username, passwd=password, db=database, port=port, use_unicode=True, charset='utf8')
         cursor = conn.cursor()
     except:
-        logging.error("could not coonnect to RDS")
+        logging.error("could not connect to rds")
         sys.exit(1)
 
     headers = get_headers(client_id, client_secret)
 
+    ## Spotify Search API
+
+    # artists = []
+    # with open('artist_list.csv') as f:
+    #     raw = csv.reader(f)
+    #     for row in raw:
+    #         artists.append(row[0])
+
+    # for a in artists:
+    #     params = {
+    #         "q": a,
+    #         "type": "artist",
+    #         "limit": "1"
+    #     }
+    #
+    #     r = requests.get("https://api.spotify.com/v1/search", params=params, headers=headers)
+    #
+    #     raw = json.loads(r.text)
+    #
+    #     artist = {}
+    #     try:
+    #         artist_raw = raw['artists']['items'][0]
+    #         if artist_raw['name'] == params['q']:
+    #
+    #             artist.update(
+    #                 {
+    #                     'id': artist_raw['id'],
+    #                     'name': artist_raw['name'],
+    #                     'followers': artist_raw['followers']['total'],
+    #                     'popularity': artist_raw['popularity'],
+    #                     'url': artist_raw['external_urls']['spotify'],
+    #                     'image_url': artist_raw['images'][0]['url']
+    #                 }
+    #             )
+    #             insert_row(cursor, artist, 'artists')
+    #     except:
+    #         logging.error('something worng')
+    #         continue
+    #
+    # conn.commit()
+
+    cursor.execute("SELECT id FROM artists")
     artists = []
-    with open('artist_list.csv') as f:
-        raw = csv.reader(f)
-        for row in raw:
-            artists.append(row[0])
 
-    for a in artists:
-        params = {
-            "q": a,
-            "type": "artist",
-            "limit": "1"
-        }
+    for (id, ) in cursor.fetchall():
+        artists.append(id)
 
-        r = requests.get("https://api.spotify.com/v1/search", params=params, headers=headers)
+    artist_batch = [artists[i: i+50] for i in range(0, len(artists), 50)]
 
+    artist_genres = []
+    for i in artist_batch:
+
+        ids = ','.join(i)
+        URL = "https://api.spotify.com/v1/artists/?ids={}".format(ids)
+
+        r = requests.get(URL, headers=headers)
         raw = json.loads(r.text)
 
-        artist = {}
-        try:
-            artist_raw = raw['artists']['items'][0]
-            if artist_raw['name'] == params['q']:
+        for artist in raw['artists']:
+            for genre in artist['genres']:
 
-                artist.update(
+                artist_genres.append(
                     {
-                        'id': artist_raw['id'],
-                        'name': artist_raw['name'],
-                        'followers': artist_raw['followers']['total'],
-                        'popularity': artist_raw['popularity'],
-                        'url': artist_raw['external_urls']['spotify'],
-                        'image_url': artist_raw['images'][0]['url']
+                        'artist_id': artist['id'],
+                        'genre': genre
                     }
                 )
-                insert_row(cursor, artist, 'artists')
-        except:
-            logging.error('something worng')
-            continue
+
+    for data in artist_genres:
+        insert_row(cursor, data, 'artist_genres')
 
     conn.commit()
+    cursor.close()
+
     sys.exit(0)
 
 
 
-    ## Spotify Search API
-    params = {
-        "q": "BTS",
-        "type": "artist",
-        "limit": "1"
-    }
+
+    try:
+        r = requests.get("https://api.spotify.com/v1/search", params=params, headers=headers)
+
+    except:
+        logging.error(r.text)
+        sys.exit(1)
+
 
     r = requests.get("https://api.spotify.com/v1/search", params=params, headers=headers)
-
-    raw = json.loads(r.text)
-
-    artist = {}
-
-    artist_raw = raw['artists']['items'][0]
-    if artist_raw['name'] == params['q']:
-
-        artist.update(
-            {
-                'id': artist_raw['id'],
-                'name': artist_raw['name'],
-                'followers': artist_raw['followers']['total'],
-                'popularity': artist_raw['popularity'],
-                'url': artist_raw['external_urls']['spotify'],
-                'image_url': artist_raw['images'][0]['url']
-            }
-        )
-
-    insert_row(cursor, artist, 'artists')
-    conn.commit()
-
-    artist_raw = raw['artists']['items'][0]
-
-    # if artist_raw['name'] == params['q']:
-    #     artist = {
-    #         'id': artist_raw['id'],
-    #         'name': artist_raw['name'],
-    #         'followers': artist_raw['followers']['total'],
-    #         'popularity': artist_raw['popularity'],
-    #         'url': artist_raw['external_urls']['spotify'],
-    #         'image_url': artist_raw['images'][0]['url']
-    #     }
-
-    # query = """
-    #     INSERT INTO artists (id, name, followers, popularity, url, image_url)
-    #     VALUES ('{}', '{}', {}, {}, '{}', '{}')
-    #     ON DUPLICATE KEY UPDATE id = '{}', name = '{}', followers = {}, popularity = {}, url ='{}', image_url ='{}'
-    # """.format(
-    #     artist['id'],
-    #     artist['name'],
-    #     artist['followers'],
-    #     artist['popularity'],
-    #     artist['url'],
-    #     artist['image_url'],
-    #     artist['id'],
-    #     artist['name'],
-    #     artist['followers'],
-    #     artist['popularity'],
-    #     artist['url'],
-    #     artist['image_url']
-    # )
-    # cursor.execute(query)
-    # conn.commit()
-    # sys.exit(0)
 
     if r.status_code != 200:
         logging.error(r.text)
@@ -135,25 +117,22 @@ def main():
             retry_after = json.loads(r.headers)['Retry-After']
             time.sleep(int(retry_after))
 
-            r = requests.get("https://api.spotify.com/v1/search",
-                             params=params, headers=headers)
+            r = requests.get("https://api.spotify.com/v1/search", params=params, headers=headers)
 
         ## access_token expired
         elif r.status_code == 401:
 
             headers = get_headers(client_id, client_secret)
-            r = requests.get("https://api.spotify.com/v1/search",
-                             params=params, headers=headers)
+            r = requests.get("https://api.spotify.com/v1/search", params=params, headers=headers)
 
         else:
             sys.exit(1)
 
+
     # Get BTS' Albums
 
-    r = requests.get(
-        "https://api.spotify.com/v1/artists/3Nrfpe0tUJi4K4DXYWgMUX/albums", headers=headers)
+    r = requests.get("https://api.spotify.com/v1/artists/3Nrfpe0tUJi4K4DXYWgMUX/albums", headers=headers)
 
-    # Receiving data
     raw = json.loads(r.text)
 
     total = raw['total']
@@ -164,9 +143,8 @@ def main():
     albums = []
     albums.extend(raw['items'])
 
-    # bring only 100 albums
-    count = 0
-    while count < 100 and next:
+    ## 난 100개만 뽑아 오겠다
+    while next:
 
         r = requests.get(raw['next'], headers=headers)
         raw = json.loads(r.text)
@@ -179,11 +157,11 @@ def main():
     print(len(albums))
 
 
+
 def get_headers(client_id, client_secret):
 
     endpoint = "https://accounts.spotify.com/api/token"
-    encoded = base64.b64encode("{}:{}".format(
-        client_id, client_secret).encode('utf-8')).decode('ascii')
+    encoded = base64.b64encode("{}:{}".format(client_id, client_secret).encode('utf-8')).decode('ascii')
 
     headers = {
         "Authorization": "Basic {}".format(encoded)
@@ -210,8 +188,10 @@ def insert_row(cursor, data, table):
     columns = ', '.join(data.keys())
     key_placeholders = ', '.join(['{0}=%s'.format(k) for k in data.keys()])
     sql = "INSERT INTO %s ( %s ) VALUES ( %s ) ON DUPLICATE KEY UPDATE %s" % (table, columns, placeholders, key_placeholders)
-    cursor.execute(sql, list(data.values())*2) # for duplicate
+    cursor.execute(sql, list(data.values())*2)
 
 
-if __name__ == '__main__':
+
+
+if __name__=='__main__':
     main()
